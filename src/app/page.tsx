645e3bed89f26b5f05d9e3f592ceb80210055110
Roberto Solares <betosolares12@gmail.com>
2024-04-16 11:20:54 -0400

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
+import { mock } from "node:test";
+
+const mockURLs = [
+  "https://utfs.io/f/1c6f2d1a-e497-4342-8a0a-2e06ae1c4270-qwjwrt.jpg",
+  "https://utfs.io/f/c4921e54-1e86-4266-86e0-2b81cc1a80dd-lkwtv9.jpeg"
+];
+
+const mockImages = mockURLs.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)!
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        [...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+          </div>
+        ))
+      }
+      </div>
     </main>
   );
 }
